48431d09c49c809612e11f38c87cb566f72e4b05
Alejandro Corredor <aecorredor93@gmail.com>
2017-05-26 17:14:48 -0400

    finish section 4 'higher order components'

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import CommentBox from './comment_box';
-import CommentList from './comment_list';
+import React from 'react';
+import { Component } from 'react';
+import Header from './header';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <CommentBox />
-        <CommentList />
+        <Header />
+        {this.props.children}
       </div>
     );
   }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-import commentsReducer from './comments';
+import authenticationReducer from './authentication';
 
 const rootReducer = combineReducers({
-  comments: commentsReducer,
+  authenticated: authenticationReducer,
 });
 
 export default rootReducer;
